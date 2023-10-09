a87438bb3e6a3d8f097e33ae51d2fe8250a0a394
G3n4r00 <rm551986@fiap.com.br>
2023-10-09 09:02:19 -0300

    estilização cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
-
+import "./Cabecalho.scss"
 export default function Cabecalho() {
   return (
     <>
-      <header>
-        <h1>Vite+React coded by - RM551986</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link> </li>
-            <li><Link to="/produtos">Produtos</Link> </li>
+      <header className= "Cabecalho">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link> </li>
+              <li><Link to="/produtos">Produtos</Link> </li>
           </ul>
         </nav>
       </header>
