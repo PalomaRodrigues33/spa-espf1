9a5d21ef5f82869cc2c9a6f7f0dfafcf77cdabf6
G3n4r00 <rm551986@fiap.com.br>
2023-10-09 09:16:51 -0300

    cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Cabecalho.scss"
+
+
 export default function Cabecalho() {
-  return (
-    <>
-      <header className= "Cabecalho">
+
+  //Use location sabe a rota que voce está no momento 
+  const rotaAtual = useLocation();
+  //Vamos comparar um coisa com a aoutra e vamos usar a classe do css para mudar a cor do link atual 
+    return (
+      <>
+        <header className="Cabecalho">
           <nav>
             <ul>
-              <li><Link to="/">Home</Link> </li>
-              <li><Link to="/produtos">Produtos</Link> </li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  );
-}
+              <li><Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>Home</Link> </li>
+              <li><Link to="/produtos" className={rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link> </li>
+            </ul>
+          </nav>
+        </header>
+      </>
+    );
+  }
+  
